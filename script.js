917e37d865860b4f69ffb3b4881230045da4e419
Marcin <mkula.poczta@gmail.com>
2023-06-12 15:13:47 +0200

    color palette added as jpg; js: variables assigned

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const username = document.querySelector('#username')
+const pass = document.querySelector('#password')
+const pass2 = document.querySelector('#password2')
+const email = document.querySelector('#email')
+const sendBtn = document.querySelector('.semd')
+const clearBtn = document.querySelector('.clear')
+const popup = document.querySelector('.popup')
+
+
+
