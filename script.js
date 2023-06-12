8cf1ace1f4076730314b5398d65fb7524e2a660c
Marcin <mkula.poczta@gmail.com>
2023-06-12 17:51:13 +0200

    show error function added

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-const username = document.querySelector('#username')
-const pass = document.querySelector('#password')
-const pass2 = document.querySelector('#password2')
-const email = document.querySelector('#email')
-const sendBtn = document.querySelector('.semd')
-const clearBtn = document.querySelector('.clear')
-const popup = document.querySelector('.popup')
+const username = document.querySelector("#username")
+const pass = document.querySelector("#password")
+const pass2 = document.querySelector("#password2")
+const email = document.querySelector("#email")
+const sendBtn = document.querySelector(".send")
+const clearBtn = document.querySelector(".clear")
+const popup = document.querySelector(".popup")
 
+const showError = (input, msg) => {
+	const formBox = input.parentElement
+    const errorMsg = formBox.querySelector('.error-text')
+    formBox.classList.add('error')
+    errorMsg.textContent = msg
 
+}
 
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === "") {
+			console.log("error")
+			showError(el, el.placeholder)
+		} else {
+			console.log("ok")
+		}
+	})
+}
+
+sendBtn.addEventListener("click", e => {
+	e.preventDefault()
+	checkForm([username, pass, pass2, email])
+})
+
+clearBtn.addEventListener("click", e => {
+	e.preventDefault()
+	;[username, pass, pass2, email].forEach(el => {
+		el.value = ""
+	})
+})
